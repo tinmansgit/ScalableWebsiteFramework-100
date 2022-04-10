ae2f860be6913d28b4b5fae48bcc358f219b478a
Route One Studio <102759745+routeonestudio@users.noreply.github.com>
2022-04-09 19:09:16 -0500

    Add files via upload

---
+++ connections/readonly_mysql_connect.php
@@ -0,0 +1,16 @@
+<?php
+
+// This file contains the read only database access information. 
+// Create a dir one level above domain.com dir and place this file.
+// Contains connection variables to connect to MySQL and selects the database.
+
+// Ensure the MySQL user has read only priveleges.
+
+// Set the database access information as constants.
+
+$db  = 'database_name';
+$host = 'localhost';
+$user = 'username';
+$pass = 'password';
+
+?>
